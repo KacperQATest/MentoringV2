c83ba8d571c866c0e5ea24601bca0d6df12986de
KacperQATest <klipczynski@sii.pl>
2023-09-03 21:20:31 +0200

    Add new solution for exercise 10-15

--- src/main/java/W3resource/StringExcercises.java
+++ src/main/java/W3resource/StringExcercises.java
@@ -1,8 +1,17 @@
 package W3resource;
 
+import java.text.spi.DateFormatProvider;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+
 public class StringExcercises {
 
     public static void main(String[] args){
+        // performance time
+        long start = System.currentTimeMillis();
+        //System.out.println(System.currentTimeMillis() - start + "ms"); It should be at the end of test or specified step
 //1. Write a Java program to get the character at the given index within the string.
 
         String str = "Java Exercises!";
@@ -35,7 +44,7 @@ public class StringExcercises {
 
         //3.Write a Java program to get the character (Unicode code point) before the specified index within the string.
 
-
+        System.out.println("Excercise 3 Berginning!!!!!!!");
         for (int f=0; f < sizeWebAddress; f++) {
             int unicodeUnits = webAddress.codePointAt(f);
             if(unicodeUnits==51 || unicodeUnits==101) {
@@ -43,18 +52,28 @@ public class StringExcercises {
                 System.out.println(webAddress.charAt(f-1));
             }
         }
-
+        System.out.println("Excercise 3 End!!!!!!!");
 
         //4.Write a Java program to count Unicode code points in the specified text range of a string.
 
+        System.out.println("Excercise 4 Berginning!!!!!!!");
 
         System.out.println("Original String : " + webAddress);
 
         // codepoint from index 1 to index 10, 10th index is not included
         int ctr = webAddress.codePointCount(1, 10);
 
+        //for loop just for checking codePointCount
+        for (int f=0; f < ctr; f++) {
+            int unicodeUnits = webAddress.codePointAt(f+1);
+            System.out.println(unicodeUnits);
+            System.out.println(webAddress.charAt(f+1));
+        }
+
         // prints character from index 1 to index 10 --- 10th index has been skiped because dot is not included in unicode ?
         System.out.println("4.Codepoint count = " + ctr);
+
+        System.out.println("Excercise 4 End!!!!!!!");
 
         //5.Write a Java program to compare two strings lexicographically.
         //Two strings are lexicographically equal if they are the same length and contain the same characters in the same positions.
@@ -101,6 +120,89 @@ public class StringExcercises {
 
 
         //10 Write a Java program to compare a given string to a specified string buffer.
+
+        String string11 = "Example";
+        String string22 = "example";
+        StringBuffer sb = new StringBuffer();
+
+        String sbString = sb.append("example").toString();
+        System.out.println(sbString);
+
+        System.out.println(string11==sbString);
+        System.out.println(string11.contentEquals(sbString));
+        System.out.println(string22==sbString);
+        System.out.println(string22.contentEquals(sbString));
+
+
+        //11. Write a Java program to create a String object with a character array.
+
+        System.out.println("Excersice 11");
+
+        char[] characters = {'1', '2', '3', '4'};
+
+        String str11 = String.copyValueOf(characters, 0, 2);
+
+        System.out.println("\nThe book contains:" + str11 + " pages");
+
+        //12.Write a Java program to check whether a given string ends with another string.
+
+        System.out.println("Excercise 12");
+
+        String correct1 = "Python Exercises";
+        String incorrect1 = "Python Exercise";
+
+        System.out.println(correct1.endsWith("se"));
+        System.out.println(incorrect1.endsWith("se"));
+
+        //13. Write a Java program to check whether two String objects contain the same data.
+        System.out.println("Excercise 13");
+        String exc13_1 = "Stephen Edwin King";
+        String exc13_2 = "Walter Winchell";
+        String exc13_3 = "stephen edwin king";
+
+        System.out.println(exc13_1.equals(exc13_2));
+        System.out.println(exc13_1.equals(exc13_3));
+
+        //14. Write a Java program to compare a given string to another string, ignoring case considerations.
+        System.out.println("Excercise 14");
+
+        String exc14_1 = "Stephen Edwin King";
+        String exc14_2 = "Walter Winchell";
+        String exc14_3 = "stephen edwin king";
+
+        System.out.println(exc13_1.equalsIgnoreCase(exc13_2));
+        System.out.println(exc13_1.equalsIgnoreCase(exc13_3));
+
+        //15. Write a Java program to print the current date and time in the specified format.
+        System.out.println("Excercise 15");
+
+        LocalDate ld = LocalDate.now();
+        System.out.println(ld);
+       // LocalDateTime ldt = LocalDateTime.now();
+        //System.out.println(ldt + "before formatting");
+
+        LocalDateTime myDateObj = LocalDateTime.now();
+        System.out.println(myDateObj + "before formatting");
+        //formatting
+        DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("MMMM-dd-yyyy HH:mm:ss");
+        String formattedDate = myDateObj.format(myFormatObj);
+        System.out.println(formattedDate + "After formatting");
+
+        Calendar c = Calendar.getInstance();
+        System.out.println("Current date and time:");
+        System.out.format("%tB %te, %tY%n", c, c, c);
+        System.out.format("%tl:%tM %tp%n", c, c, c);
+
+
+
+
+
+
+
+
+
+
+
 
 
 
